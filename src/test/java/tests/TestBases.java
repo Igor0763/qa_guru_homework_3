608ad05777a8ac7de62cbc5a5e806d7c1b38aa9a
Igor0763 <Igor763763763@yandex.ru>
2024-01-09 13:35:01 +0400

    fix tests

--- src/test/java/tests/TestBases.java
+++ src/test/java/tests/TestBases.java
@@ -14,7 +14,7 @@ public class TestBases {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://selenoid.autotests.cloud/";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
